a9e2b6b7c466fd9bfc93d836d0b2dd7160846bd2
rika2017 <18969043531@163.com>
2018-09-26 18:08:30 +0800

    2222222222222222222

--- src/test/java/mysite/TestLoginServlet.java
+++ src/test/java/mysite/TestLoginServlet.java
@@ -1,14 +1,20 @@
 package mysite;
 
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
 import com.login.LoginServlet;
 
 public class TestLoginServlet {
- private LoginServlet ss; 
- public void TestLoginServlet() {
-	ss = new LoginServlet();
- }
+	private LoginServlet ss; 
  
- public void test() {
-	 ss.mytest();
- }
+	@BeforeClass
+	public void ss() {
+		ss = new LoginServlet();
+	}
+	
+	@Test
+	public void test() {
+		ss.mytest();
+	}
 }
